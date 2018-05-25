d2f1b5e8f2dd47abc3e209d1b871bf59c1f59e20
Panton1986 <39476284+Panton1986@users.noreply.github.com>
2018-05-25 09:08:20 +0300

    Update Homework3.java

--- Homework3.java
+++ Homework3.java
@@ -3,25 +3,29 @@ import java.util.Scanner;
  
 public class Main  {
   public static Scanner sc = new Scanner(System.in);
-
+  
 	public static void main(String args[]){
 	Random rand = new Random();
   int a = rand.nextInt (10);
   System.out.println(a);
-  
-  int d;
-  
-  do {
+   
+  int i;
+  int d; 
+//   for (i = 3; i >= 1; i-=1){ 
+    do {
     d = num ("Введите число от 0 до 9", 0, 9);
     System.out.println("Вы ввели число = " + d);
-
-    if (d == a) System.out.println("Угадал");
-    else if (d < a) System.out.println("Неверно, Загаданное число больше");
-    else if (d > a) System.out.println("Неверно, Загаданное число меньше");
-    else System.out.println("не Угадал");
-  }
-  while (d != a);
+   
+      if (d == a) System.out.println("Угадал");
+      else if (d < a) System.out.println("Неверно, Загаданное число больше ");
+      else if (d > a) System.out.println("Неверно, Загаданное число меньше ");
+      else System.out.println("не Угадал");
+    }  while (d != a);
+//   }  
+ 
   
+//  System.out.println("Осталось попыток = " + i);
+       
 }
 
   public static int num (String massage, int min, int max){
@@ -36,3 +40,5 @@ public class Main  {
 
 
 }
+
+ 
